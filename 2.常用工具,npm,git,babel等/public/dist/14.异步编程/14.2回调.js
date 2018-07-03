360cc58daa0428d76023c4d529bb3c8bba8ad2ba
xiaoquanquanxin <xxkl123@126.com>
2018-07-03 15:51:59 +0800

    第14章 异步编程 promise

--- 2.常用工具,npm,git,babel等/public/dist/14.异步编程/14.2回调.js
+++ 2.常用工具,npm,git,babel等/public/dist/14.异步编程/14.2回调.js
@@ -1,3 +1,5 @@
+'use strict';
+
 //  回调就是一个函数,在你需要的时候调用他.通常会把这些回调函数作为参数传递给需求函数
 
 function a(b, c) {
@@ -7,7 +9,7 @@ function a(b, c) {
 setTimeout(a, 3, '111', 'cc');
 
 //  每当一个函数被调用时,都创建了一个闭包,所有在函数内部创建的变量,只有在被访问的时候才存在.
-let i;
+var i = void 0;
 //  每次都是12
 for (i = 1; i < 12; i++) {
     setTimeout(function () {
@@ -16,17 +18,22 @@ for (i = 1; i < 12; i++) {
 }
 
 //  let j 在循环中定义
-for (let j = 1; j < 12; j++) {
+
+var _loop = function _loop(j) {
     setTimeout(function () {
-        console.log(j)
+        console.log(j);
     }, 10 * j);
+};
+
+for (var j = 1; j < 12; j++) {
+    _loop(j);
 }
 //  相当于
-let k;
+var k = void 0;
 for (k = 1; k < 12; k++) {
     (function (i) {
         setTimeout(function () {
             //console.log(i)
         }, k * 10);
-    }(k));
+    })(k);
 }
---
+++ 2.常用工具,npm,git,babel等/public/es6/14.异步编程/14.3承诺.js
@@ -0,0 +1,30 @@
+//  promise 确保回调始终被可以预期的方式处理
+//  创建promise
+function countDown(seconds) {
+    return new Promise(function (resolve, reject) {
+        for (let i = seconds; i >= 0; i--) {
+            setTimeout(function () {
+                if (i === 13) {
+                    return reject(new Error("报错"));
+                } else if (i > 0) {
+                    console.log(i);
+                } else {
+                    resolve(console.log("GO"));
+                }
+            }, i * 222);
+        }
+    })
+}
+//  使用promise
+let cou1 = countDown(3).then(function () {
+    console.log("countDown 完整成功");
+}, function (err) {
+    console.log("countDown 出错了" + err);
+});
+let cou2 = countDown(3);
+cou2.then(function () {
+    console.log("countDown 完整成功");
+});
+cou2.catch(function (err) {
+    console.log("countDown 出错了" + err);
+});
