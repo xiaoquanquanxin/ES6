5de1305c631376f0729c90b55bea15c2c9549a86
xiaoquanquanxin <xxkl123@126.com>
2018-09-28 19:04:36 +0800

    babel

--- 2.常用工具,npm,git,babel等/public/es6/export/app.js
+++ 2.常用工具,npm,git,babel等/public/es6/export/app.js
@@ -1,6 +1,8 @@
 //import * as math from "./math";
 //console.log("2π = " + math.sum(math.pi, math.pi));
 
-import  nba ,{pi, e} from "./mathplugin";
-//console.log(exp,pi,e);
-console.log("e^π = " + nba(pi));
+//import  nba ,{pi, e} from "./mathplugin";
+//console.log("e^π = " + nba(pi));
+
+import * as math from './mathplugin';
+console.log(math);
--- 2.常用工具,npm,git,babel等/public/es6/export/mathplugin.js
+++ 2.常用工具,npm,git,babel等/public/es6/export/mathplugin.js
@@ -3,4 +3,3 @@ export var e = 2.71828182846;
 export default function (x) {
     return Math.random() * Math.exp(x);
 };
-console.log(exports);
