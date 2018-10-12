b732f925cb588769222236cba3249a4db5ece89b
xiaoquanquanxin <xxkl123@126.com>
2018-10-12 16:50:47 +0800

    es6阮一峰 字符创扩展

--- ecmascript2015入门,阮一峰/字符串的扩展.js
+++ ecmascript2015入门,阮一峰/字符串的扩展.js
@@ -19,3 +19,26 @@
 {
     console.log('\u01D1'.normalize() === '\u004F\u030C'.normalize());
 }
+
+//  参数表示开始搜索的位置
+{
+    let s = 'hello';
+    s.includes('h');
+    s.endsWith('e', 2);
+    s.startsWith('h');
+}
+//  重复,参数取整,不可<-1,字符串参数先转为数字
+{
+    let s = 'ncaa';
+    s.repeat(2);
+    s.repeat('0.2');
+}
+//  如果某个字符串不够指定长度，会在头部或尾部补全。
+{
+    let x = 'x';
+    x.padStart(5, 'ab'); // 'ababx'
+    x.padStart(4, 'ab'); // 'abax'
+
+    x.padEnd(5, 'ab'); // 'xabab'
+    x.padEnd(4, 'ab'); // 'xaba'
+}
