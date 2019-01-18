3464705f56c34fb51f2189f3f8bb0bde373c1a87
xiaoquanquanxin <xxkl123@126.com>
2019-01-18 10:08:35 +0800

    es6阮一峰  Generator 生成器

--- ecmascript2015入门,阮一峰/Generator 函数的异步应用.js
+++ ecmascript2015入门,阮一峰/Generator 函数的异步应用.js
@@ -6,15 +6,16 @@
     let s = sum(1 + 2, 3);
     console.log(s);
     //  thunk
-    let thunk = (x)=>x++;
+    let thunk = (x)=>x;
     let t = sum(thunk(1 + 2), 3);
     console.log(t);
 }
 
 
 {
-    var r1 = g.next();
-    r1.value(fn);
+
+    //var r1 = g.next();
+    //r1.value(fn);
     function fn(err, data) {
         if (err) throw err;
         r1 = g.next(data);
